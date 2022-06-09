c2d4ac032bd0b41b41471d450c724e045dbc3426
Larissa Ramos <68797820+larisramos@users.noreply.github.com>
2022-06-09 11:55:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from tkinter import *
+top = Frame() ; top.pack()
+rotulo = Label (top,text="Rótulo Exemplo", foreground="blue")
+rotulo.pack()
+rotulo.configure(relief="ridge", font="Arial 24 bold", border=5, background="yellow")
